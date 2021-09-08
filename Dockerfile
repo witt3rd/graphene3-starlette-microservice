53a3cd34d07412d0c40c9d4f075d5c8d36ce7656
witt3rd <witt3rd@witt3rd.com>
2021-09-07 17:04:52 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:3.9.7-slim-buster
+
+RUN apt-get update && apt-get upgrade -y
+
+RUN pip3 install gunicorn uvloop httptools
+
+COPY requirements.txt /app/requirements.txt
+RUN pip3 install -r /app/requirements.txt
+
+COPY app/ /app
+
+ENV ACCESS_LOG=${ACCESS_LOG:-/proc/1/fd/1}
+ENV ERROR_LOG=${ERROR_LOG:-/proc/1/fd/2}
+
+ENTRYPOINT /usr/local/bin/gunicorn main:app \
+    -b 0.0.0.0:3000 \
+    -w 4 \
+    -k uvicorn.workers.UvicornWorker \
+    --chdir /app \
+    --access-logfile "$ACCESS_LOG" \
+    --error-logfile "$ERROR_LOG"
---
+++ app/main.py
@@ -0,0 +1,54 @@
+import asyncio
+
+import graphene
+from graphene_file_upload.scalars import Upload
+
+from starlette.requests import HTTPConnection, Request
+from starlette.responses import HTMLResponse, JSONResponse, Response
+from starlette.applications import Starlette
+
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    me = graphene.Field(User)
+
+    def resolve_me(root, info):
+        return {"id": "john", "name": "John"}
+
+
+class FileUploadMutation(graphene.Mutation):
+    class Arguments:
+        file = Upload(required=True)
+
+    ok = graphene.Boolean()
+
+    def mutate(self, info, file, **kwargs):
+        return FileUploadMutation(ok=True)
+
+
+class Mutation(graphene.ObjectType):
+    upload_file = FileUploadMutation.Field()
+
+
+class Subscription(graphene.ObjectType):
+    count = graphene.Int(upto=graphene.Int())
+
+    async def subscribe_count(root, info, upto=3):
+        for i in range(upto):
+            yield i
+            await asyncio.sleep(1)
+
+
+app = Starlette()
+schema = graphene.Schema(query=Query, mutation=Mutation,
+                         subscription=Subscription)
+
+app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))  # Graphiql IDE
+
+# app.mount("/", GraphQLApp(schema, on_get=make_playground_handler()))  # Playground IDE
+# app.mount("/", GraphQLApp(schema)) # no IDE
